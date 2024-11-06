500c5414690c105b9ccff81cf4ac647ca7bb9a6b
kanyui <lowkanyui@gmail.com>
2024-11-06 21:49:04 +0800

    Fixed HistoryLogs

--- routerunner/server/server.js
+++ routerunner/server/server.js
@@ -558,6 +558,20 @@ app.get('/api/user/jobCompleted', async (req,res) => {
     }, {$set:{
       status: 'completed',
     }});
+
+    // handle history logs
+    const timestamp = Date.now();
+    const date = new Date(timestamp);
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    const formattedDate = `${day}/${month}/${year}`;
+    const newlog = new HistoryLogs({
+      job: jobcompleted._id,
+      date: formattedDate
+    });
+    await newlog.save();
+
     const jobongoingupdate = await Job.updateOne({
       status: 'waiting',
     }, {$set:{
