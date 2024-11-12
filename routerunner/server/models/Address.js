fa7893aca0bf3c29754e56ca9f573c54bcf177e8
AuroraVane <47216870+AuroraVane@users.noreply.github.com>
2024-11-12 23:26:14 +0800

    Update Address.js

--- routerunner/server/models/Address.js
+++ routerunner/server/models/Address.js
@@ -5,7 +5,7 @@ const addressSchema = new mongoose.Schema({
     street: {type: String, required: false, unique: false},
     block: { type: String, required: false, unique: false},
     unitNumber: { type: String, required: false, unique: false},
-    postalCode: { type: Number, required: true, unique: false},
+    postalCode: { type: String, required: true, unique: false},
 });
 
 module.exports = mongoose.model('Address', addressSchema);
